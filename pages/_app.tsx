b13c5ddf73921e8c1e26e6784a93ccf8e357aff7
Giulio Poggia <giulio.poggia@hinto.com>
2022-12-05 15:49:12 +0100

    moved general head from document to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { GlobalStyle } from "../styles/GlobalStyles";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Xmas Hinto</title>
+        <meta name="description" content="xmas.hinto" />
+        <link rel="shortcut icon" href="/favicon-hinto.gif" />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,7 @@ import { notoFont } from "../components/commons/Theme";
 export default function Document() {
   return (
     <Html>
-        <Head>
-        <title>Xmas Hinto</title>
-        <meta name="description" content="xmas.hinto" />
-        <link rel="shortcut icon" href="/favicon-hinto.gif" />
-      </Head>
+      <Head />
       <body className={notoFont.className}>
         <Main />
         <NextScript />
