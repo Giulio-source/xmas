4c232a9584b90c0f82292c90e5be07fe230510d4
Giulio Poggia <giulio.poggia@hinto.com>
2022-12-13 15:38:26 +0100

    hopefully fixes routing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   reactStrictMode: false,
   swcMinify: true,
   compiler: {
     styledComponents: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
